3449cf3b42e079f0ec3756697da69b76ab1cd224
ydb <h-yidb@voyah.com.cn>
2026-06-17 09:23:29 +0800

    两者结合前

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # 这是一个示例 Python 脚本。
+import pandas as pd
+
 
 # 按 Shift+F10 执行或将其替换为您的代码。
 # 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
@@ -12,5 +14,7 @@ def print_hi(name):
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
     print_hi('PyCharm')
+    pd.read_csv('train_server.py')
+
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
